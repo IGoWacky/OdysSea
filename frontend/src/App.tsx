45c3c142604554f41f16c135d8b0fc8861905946
sekun <sekunoshikanlu@gmail.com>
2026-01-26 13:58:27 -0500

    CORS updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import { useState, useEffect } from 'react'
 
 function App() {
+  let count:number = 0
+
+  const [data, setData] = useState<string>("null")
+
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+      .then((response) => response.text())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error))
+  }, [])
+
   return (
     <>
-      <h1 className="text-2xl font-bold">OdysSea</h1>
+      <h1 className="text-2xl font-bold">OdysSea {count}</h1>
+      <p>{data}</p>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 const port = process.env.PORT || 3000;
+const allowedOrigins = ['http://localhost:5173'];
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-    console.log('Received request at /');
-    res.send('OdysSea Server!');
+  console.log('Received request at /');
+  res.send('Hey OdysSea Server!');
 });
 
 app.listen(port, () => {
